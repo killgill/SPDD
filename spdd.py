db4f3495b488b38e3e9ca60cf24f50829813afc4
Karan Gill <gill@cooper.edu>
2018-11-02 00:41:53 -0700

    some stuff

--- spdd.py
+++ spdd.py
@@ -42,54 +42,45 @@ GPIO.add_event_detect(23, GPIO.RISING, callback=doAClick, bouncetime=20) # Beer,
 try:
     while True:
 
-        SCOPES = 'https://www.googleapis.com/auth/spreadsheets'
-        store = file.Storage('token.json')
-        creds = store.get()
-        if not creds or creds.invalid:
-            flow = client.flow_from_clientsecrets('credentials.json', SCOPES)
-            creds = tools.run_flow(flow, store)
-        service = build('sheets', 'v4', http=creds.authorize(Http()))
-        SPREADSHEET_ID = '1hopTf_z_OzquBngV11XTryX9qX4AiYPi1hsOucpfVbk'
-        ID_NUMBER_RANGE_NAME = 'number_of_ids!A1'
-        VALUE_RENDER_OPTION = 'UNFORMATTED_VALUE'
-        num_ids_response = service.spreadsheets().values().get(spreadsheetId=SPREADSHEET_ID, range=ID_NUMBER_RANGE_NAME, valueRenderOption=VALUE_RENDER_OPTION).execute()
-        num_ids = num_ids_response['values'][0][0]
-        print(num_ids)
-        print(type(num_ids))
-        ID_NUMBERS_RANGE = 'ids!A2:A' + str(num_ids+1)
-        VALUE_RENDER_OPTION = 'UNFORMATTED_VALUE'
-        ids_list_response = service.spreadsheets().values().get(spreadsheetId=SPREADSHEET_ID, range=ID_NUMBERS_RANGE, majorDimension='COLUMNS').execute()
-        pprint(ids_list_response)
-        ids_list = ids_list_response['values'][0]
-        print(ids_list)
-        print(type(ids_list))
+        def onSwipe(card_id):
+            SCOPES = 'https://www.googleapis.com/auth/spreadsheets'
+            store = file.Storage('token.json')
+            creds = store.get()
+            if not creds or creds.invalid:
+                flow = client.flow_from_clientsecrets('credentials.json', SCOPES)
+                creds = tools.run_flow(flow, store)
+            service = build('sheets', 'v4', http=creds.authorize(Http()))
+            SPREADSHEET_ID = '1hopTf_z_OzquBngV11XTryX9qX4AiYPi1hsOucpfVbk'
+            ID_NUMBER_RANGE_NAME = 'number_of_ids!A1'
+            VALUE_RENDER_OPTION = 'UNFORMATTED_VALUE'
+            num_ids_response = service.spreadsheets().values().get(spreadsheetId=SPREADSHEET_ID, range=ID_NUMBER_RANGE_NAME, valueRenderOption=VALUE_RENDER_OPTION).execute()
+            num_ids = num_ids_response['values'][0][0]
+            print(num_ids)
+            print(type(num_ids))
+            ID_NUMBERS_RANGE = 'ids!A2:A' + str(num_ids+1)
+            VALUE_RENDER_OPTION = 'UNFORMATTED_VALUE'
+            ids_list_response = service.spreadsheets().values().get(spreadsheetId=SPREADSHEET_ID, range=ID_NUMBERS_RANGE, majorDimension='COLUMNS').execute()
+            pprint(ids_list_response)
+            ids_list = ids_list_response['values'][0]
+            print(ids_list)
+            print(type(ids_list))
 
-        print(type(ids_list[0]))
-        card_id = u'3453909285' # placeholder
-        print(type(card_id))
-        print('guap1')
-        if card_id in ids_list:
-            id_index = ids_list.index(card_id)
-        print(ids_list[0] == card_id)
-        print(ids_list[0])
-        print(card_id)
-        print('guap2')
-        BEERS_FOR_CARD = 'ids!C' + str(id_index+2)
-        print('guap3')
-        cur_beers_response = service.spreadsheets().values().get(spreadsheetId=SPREADSHEET_ID, range=BEERS_FOR_CARD, valueRenderOption=VALUE_RENDER_OPTION).execute()
-        print('guap4')
-        cur_beers = cur_beers_response['values'][0][0]
-        print('guap5')
-        new_beers = {'values': [[cur_beers + 1]]}
-        print('guap6')
-        new_beers_resp = service.spreadsheets().values().update(spreadsheetId=SPREADSHEET_ID, range=BEERS_FOR_CARD, body=new_beers, valueInputOption='RAW').execute()
-        print('guap7')
-        pprint(new_beers_resp)
-        print('guap8')
+            print(type(ids_list[0]))
+            card_id = u'3453909285' # placeholder
+            print(type(card_id))
+            if card_id in ids_list:
+                id_index = ids_list.index(card_id)
+            print(ids_list[0] == card_id)
+            print(ids_list[0])
+            print(card_id)
+            BEERS_FOR_CARD = 'ids!C' + str(id_index+2)
+            cur_beers_response = service.spreadsheets().values().get(spreadsheetId=SPREADSHEET_ID, range=BEERS_FOR_CARD, valueRenderOption=VALUE_RENDER_OPTION).execute()
+            cur_beers = cur_beers_response['values'][0][0]
+            new_beers = {'values': [[cur_beers + 1]]}
+            new_beers_resp = service.spreadsheets().values().update(spreadsheetId=SPREADSHEET_ID, range=BEERS_FOR_CARD, body=new_beers, valueInputOption='RAW').execute()
+            pprint(new_beers_resp)
 
-        print(type(swipe))
         swipe = raw_input("ID#")
-        print(type(swipe))
         if isinstance(swipe, basestring):
             print(swipe)
         '''
